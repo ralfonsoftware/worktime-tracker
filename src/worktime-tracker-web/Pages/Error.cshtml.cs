22c8a5b2ee342e00bec4d7e1ebdb10d176acc4aa
Ralf Ehlert <coding@ralfonsoftware.de>
2025-04-21 15:51:20 +0200

    Base structure Create index razor pages for the main areas

--- src/worktime-tracker-web/Pages/Error.cshtml.cs
+++ src/worktime-tracker-web/Pages/Error.cshtml.cs
@@ -2,7 +2,7 @@ using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace worktime_tracker_web.Pages;
+namespace WorktimeTracker.Web.Pages;
 
 [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
 [IgnoreAntiforgeryToken]
---
+++ src/worktime-tracker-web/Pages/SickDays/Index.cshtml.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WorktimeTracker.Web.Pages.SickDays;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+    }
+}
